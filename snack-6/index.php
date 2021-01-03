6beec790ac7b424759bcb6a7cef78917d6c8230f
Martina <martina.pozzerle@libero.it>
2021-01-03 13:05:33 +0100

    #snacks 1-2-3-4-5-6 completati

--- snack-6/index.php
+++ snack-6/index.php
@@ -24,14 +24,14 @@ e i PM in un rettangolo verde-->
     ]
 ];
 echo "<div style='border: 5px solid grey; width: 100px; height: 100px;'>";
-foreach($db["teachers"] as $k => $v){
-    echo($v["name"]. " ". $v["lastname"]. "<br>");
+foreach($db["teachers"] as $k){
+    echo($k["name"]. " ". $k["lastname"]. "<br>");
 }
 echo "</div>";
 
 echo "<div style='border: 5px solid green; width: 100px; height: 100px; margin-top: 100px;'>";
-foreach($db["pm"] as $k => $v){
-    echo($v["name"]. " " .$v["lastname"]. "<br>");
+foreach($db["pm"] as $k){
+    echo($k["name"]. " " .$k["lastname"]. "<br>");
 }
 echo "</div>";
 
